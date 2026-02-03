2aff9e8b62efdeaa2d8cbed0d15bbd2df4340e15
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2026-02-03 11:00:37 +0700

    CRUD in FE

--- main.js
+++ main.js
@@ -1,65 +1,67 @@
-
-let Student = function (name, family, age, scores, birth) {
-    this.name = name;
-    this.family = family;
-    this.age = age;
-    this.scores = scores;
-    this.birth = birth;
-    this.getFullName = function () {
-        return this.family + " " + this.name
-    },
-        this.getFullBirthDay = function () {
-            return `${this.birth.day}/${this.birth.month}/${this.birth.year}`
-        },
-        this.getAVGScore = function () {
-            return this.scores.reduce(
-                function (sum, e) {
-                    return sum += e
-                }
-            ) / this.scores.length;
+async function getData() {
+    try {
+        let res = await fetch('http://localhost:3000/posts');
+        let posts = await res.json();
+        let body = document.getElementById('table_body');
+        body.innerHTML = '';
+        for (const post of posts) {
+            body.innerHTML += `<tr>
+                <td>${post.id}</td>
+                <td>${post.title}</td>
+                <td>${post.views}</td>
+                <td><input type='submit' value='Delete' onclick='
+                Delete(${post.id})'></td>
+            </tr>`
         }
+    } catch (error) {
+        console.log(error);
+    }
 }
-let DateO = function (year, month, day) {
-    this.year = year;
-    this.month = month;
-    this.day = day
+async function Save() {
+    let id = document.getElementById('txt_id').value;
+    let title = document.getElementById('txt_title').value;
+    let views = document.getElementById('txt_views').value;
+    let getItem = await fetch('http://localhost:3000/posts/' + id);
+    if (getItem.ok) {
+        //edit
+        let res = await fetch('http://localhost:3000/posts/'+id, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                views: views
+            })
+        })
+        if (res.ok) {
+            console.log("thanh cong");
+        }
+    } else {
+        //create
+        let res = await fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id: id,
+                title: title,
+                views: views
+            })
+        })
+        if (res.ok) {
+            console.log("thanh cong");
+        }
+    }
 }
-let student1 = new Student("Tung", "Nguyen", 18, [10, 9, 8], new DateO(2008, 1, 1))
-let student2 = new Student("Toan", "Bui", 18, [7, 6, 3], new DateO(2008, 1, 1))
-let student3 = new Student("Tung", "Nguyen", 20, [4, 3, 2], new DateO(2006, 1, 1))
-let student4 = new Student("Tung", "Bui", 20, [4, 3, 2], new DateO(2006, 1, 1))
-let array = [];
-(a, b) => a + b
-array.push(student1);
-array.push(student2);
-array.push(student3);
-array.push(student4);
-let result = array.map(
-    (e) => ({
-        fullName: e.getFullName(),
-        birth: e.getFullBirthDay(),
-        AVGScore: e.getAVGScore()
+async function Delete(id) {
+    let res = await fetch('http://localhost:3000/posts/' + id, {
+        method: 'delete'
     })
-)
-result = array.find(
-    function (e) {
-        for (const score of e.scores) {
-            if (score < 2) {
-                return true;
-            }
-        }
+    if (res.ok) {
+        console.log("xoa thanh cong");
     }
-)
-array.sort(
-    function (a, b) {
-        if (a.name.toLowerCase() == b.name.toLowerCase()) {
-            if (a.family.toLowerCase() == b.family.toLowerCase()){
-                return a.age-b.age;
-            }
-            return a.family.toLowerCase().localeCompare(b.family.toLowerCase())
-        }
-        return a.name.toLowerCase().localeCompare(b.name.toLowerCase())
-    }
-)
-console.log(array);
-//map,sort,filter,every, reduce,some
+}
+getData();
+
